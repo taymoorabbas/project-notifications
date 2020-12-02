8c4f7b42fa305b0096fc85eb6d1143b07e201bdb
taymoorabbas <www.taymoor@hotmail.com>
2020-12-03 03:23:44 +0500

    added notification on click, a button on click and colors

--- app/src/main/java/com/taytech/notificationstutorial/MainActivity.java
+++ app/src/main/java/com/taytech/notificationstutorial/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -35,6 +38,31 @@ public class MainActivity extends AppCompatActivity {
         String title = this.editTextTitle.getText().toString().trim();
         String message = this.editTextMessage.getText().toString().trim();
 
+        //notification on click===============================================
+
+        Intent intentActivity = new Intent(this, MainActivity.class);
+
+        //creating an 'appending intent' for on clicks on notification
+        PendingIntent intentContent = PendingIntent.getActivity(
+                this
+                , 0
+                , intentActivity
+                , 0);
+
+        //notification button on click==========================================
+
+        //creating a broadcast intent
+        Intent intentBroadCast = new Intent(this, NotificationReceiver.class);
+        intentBroadCast.putExtra("toastMessage", message);
+
+        PendingIntent intentAction = PendingIntent.getBroadcast(
+                this
+                , 0
+                , intentBroadCast
+                , PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //end====================================================================
+
         //note setPriority is only required for API < 26
         Notification notification = new NotificationCompat
                 .Builder(this, CHANNEL_1_ID)
@@ -43,6 +71,11 @@ public class MainActivity extends AppCompatActivity {
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setOnlyAlertOnce(true) //will not disturb when notification content updates
+                .setAutoCancel(true) // dismiss on click
+                .setContentIntent(intentContent) //attaching the intent for on click
+                .addAction(R.mipmap.ic_launcher, "Click me", intentAction) //attaching a button
                 .build();
 
         //same id each time would delete previous notification.
